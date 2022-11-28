2de1afa06f698cd66325ef025fdd349bb03779fc
hieuhthh <nguyenmautronghieu@gmail.com>
2022-11-28 04:03:34 +0000

    12321312313

--- unzip.py
+++ unzip.py
@@ -1,4 +1,7 @@
 import zipfile
+from zipfile import ZipFile
+from concurrent.futures import ThreadPoolExecutor
+import time
 
 from utils import *
 
@@ -7,18 +10,26 @@ globals().update(settings)
 
 from_dir = path_join(route, 'download')
 des = path_join(route, 'unzip')
-mkdir(des)
+force_mkdir(des)
+
+def fast_unzip(zip_path, out_path):
+    start = time.time()
+    with ZipFile(zip_path, 'r') as handle:
+        with ThreadPoolExecutor(2) as exe:
+            _ = [exe.submit(handle.extract, m, out_path) for m in handle.namelist()]
+    print('Unzip', zip_path, 'Time:', time.time() - start)
 
 filename = 'vnceleb'
-zip_file = path_join(from_dir, filename + '.zip')
-with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-    zip_ref.extractall(des)
+zip_path = path_join(from_dir, filename + '.zip')
+fast_unzip(zip_path, des)
 
-filename = 'gnv_dataset'
-zip_file = path_join(from_dir, filename + '.zip')
-with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-    zip_ref.extractall(des)
+# with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+#     zip_ref.extractall(des)
 
-zip_file = '/home/lap14880/face_bucket_huy/glint360k_224_copy.zip'
-with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-    zip_ref.extractall(des)
+# filename = 'gnv_dataset'
+# zip_path = path_join(from_dir, filename + '.zip')
+# fast_unzip(zip_path, des)
+
+# zip_path = '/home/lap14880/face_bucket_huy/glint360k_224_copy.zip'
+# fast_unzip(zip_path, des)
+
--- unzip.sh
+++
@@ -1,6 +0,0 @@
-# unzip download/ms1m_part1.zip -d download
-# unzip download/ms1m_part2.zip -d download
-# unzip download/vnceleb.zip -d download
-# unzip download/with_crop_client1706.zip -d download
-# unzip /home/lap14880/face_bucket_huy/glint360k_224_copy.zip -d download
-tar -xf /home/lap14880/face_bucket_huy/train_msra.tar.gz -C download
